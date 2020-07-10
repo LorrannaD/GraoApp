cc85cca81717efbfb55ebc233406b7e6a11fef29
LorrannaD <lorranna.developer@gmail.com>
2020-07-09 21:21:30 -0300

    melhoria na formula do codigo

--- app/src/main/java/br/com/desafiogro/ui/model/SimulatorActivity.kt
+++ app/src/main/java/br/com/desafiogro/ui/model/SimulatorActivity.kt
@@ -5,7 +5,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.com.desafiogro.R
 import kotlinx.android.synthetic.main.activity_simulador.*
-import java.util.*
 
 
 class SimulatorActivity : AppCompatActivity() {
@@ -15,16 +14,16 @@ class SimulatorActivity : AppCompatActivity() {
         setContentView(R.layout.activity_simulador)
 
         btn_simular.setOnClickListener{
-            if (editNumber_investir.text.toString().length === 0) {
-                editNumber_guardar.text.get(0)
+            if (editNumber1.text.toString().length === 0) {
+                editNumber2.text.get(0)
             }
-            if (editNumber_guardar.text.toString().length === 0) {
-                editNumber_investir.text.get(0)
+            if (editNumber2.text.toString().length === 0) {
+                editNumber1.text.get(0)
             }
-            val num1 = editNumber_investir.text.toString().toInt()
-            val num2  = editNumber_guardar.text.toString().toInt()
-            var rate : Double = 4.25
-            val sum =  (num1 * num2 ) /252 * rate
+            val num1 = editNumber1.text.toString().toInt()
+            val num2  = editNumber2.text.toString().toInt()
+            var taxaselic : Double = 4.25
+            val sum =  taxaselic + (1 + num1 + num2/252)
             textView_resultado.text = "Resultado:" + sum.toString()
             Toast.makeText(this, "Resultado" + textView_resultado.text, Toast.LENGTH_LONG).show()
         }
